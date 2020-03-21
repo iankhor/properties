02fa8f159c5e201040f8985b8e8f068b621bb2de
iankhor <iankhorbc.thecoder@gmail.com>
2020-03-21 19:32:50 +1100

    Disable react dev tools for production

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import Container from 'components/Container';
+
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 const rootElement = document.getElementById('react-app');
 

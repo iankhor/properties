1b0a44712c572bbd53e5b57ecf5aaeccb5d32dc9
iankhor <iankhorbc.thecoder@gmail.com>
2020-03-22 00:16:41 +1100

    Styled error component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,10 +1,16 @@
 import React, { Fragment } from 'react';
 
+import errorStyles from 'styles/error.css';
+
 const Error = ({ fetch }) => (
-  <Fragment>
-    <div>Uh oh. It looks like some things haven't loaded correctly</div>
-    <div data-testid="refetch" onClick={fetch}></div>
-  </Fragment>
+  <div>
+    <div className={errorStyles.error}>
+      Uh oh. It looks like some things haven't loaded correctly
+      <div data-testid="refetch" className={errorStyles.refetch} onClick={fetch}>
+        X
+      </div>
+    </div>
+  </div>
 );
 
 export default Error;

52f4271e809794e805deaad3e78c5cd7168acfd3
iankhor <iankhorbc.thecoder@gmail.com>
2020-03-22 01:37:29 +1100

    More CSS styling to error component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,13 +1,13 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 import errorStyles from 'styles/error.css';
 
 const Error = ({ fetch }) => (
   <div>
     <div className={errorStyles.error}>
-      Uh oh. It looks like some things haven't loaded correctly
+      &#128553; Uh oh. It looks like some things haven't loaded correctly
       <div data-testid="refetch" className={errorStyles.refetch} onClick={fetch}>
-        X
+        Click here to try again.
       </div>
     </div>
   </div>

5c80a4db9c1f1099058c04d2be468a24f07a4905
iankhor <iankhorbc.thecoder@gmail.com>
2020-04-12 12:19:15 +1000

    convert util.js to typescript

---
+++ src/lib/utils.ts
@@ -0,0 +1,20 @@
+type Data = {
+  [key: string]: string;
+};
+
+export const isFound = (data: Data, key: string, value: string): boolean => data.hasOwnProperty(key) && data[key] === value;
+
+const toTitleCase = (str: string): string => {
+  const s = str.toLowerCase().split(' ');
+  for (var i = 0; i < s.length; i++) {
+    s[i] = s[i].charAt(0).toUpperCase() + s[i].slice(1);
+  }
+  return s.join(' ');
+};
+
+export const snakeCaseToTitleCase = (snakeStr: string): string => {
+  return snakeStr
+    .split('_')
+    .map((str) => toTitleCase(str))
+    .join(' ');
+};
